9f14f1f1cb2208abbfed34c9cbbfa023a0e291e1
danielbratec <danielmoraissoares317@gmail.com>
2025-01-27 19:26:47 -0300

    Add files via upload

---
+++ wifi.py
@@ -0,0 +1,12 @@
+if wifi there is no error:
+    print ("Bratec Corporation")
+    print ("() Bratec Wifi v1 (r85)")
+    print ("/_\ automated wireless auditor")
+    print ("/__\ designed for microsoft windows")
+    print ("/____\")
+    print ("/     \")
+    print ("[i]to use in python press:")
+    print ("Linux: git clone https://github.com/danielbratec/wifi.py")
+
+else:
+print ("[!] the wifi.py not be boot")
